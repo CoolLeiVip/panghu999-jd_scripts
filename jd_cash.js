b4752937e02846fb232bd07ea7ebf0886b4f8408
panghu999 <51199175+panghu999@users.noreply.github.com>
2021-06-22 15:47:42 +0800

    Update jd_cash.js

--- jd_cash.js
+++ jd_cash.js
@@ -31,7 +31,7 @@ let helpAuthor = true;
 const randomCount = $.isNode() ? 5 : 5;
 let cash_exchange = false;//是否消耗2元红包兑换200京豆，默认否
 const inviteCodes = [
-  `eU9YEIzvIL9XlTmXuAhW`
+  `eU9YEIzvIL9XlTmXuAhW@eU9YarjmN_l0-GaDzHIVgA`
 ]
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
