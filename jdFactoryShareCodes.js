83d940bf184b80829aa0e3fda42db6a58bfa81f7
panghu999 <51199175+panghu999@users.noreply.github.com>
2021-06-25 09:34:30 +0800

    Update jdFactoryShareCodes.js

--- jdFactoryShareCodes.js
+++ jdFactoryShareCodes.js
@@ -29,7 +29,7 @@ if (process.env.DDFACTORY_SHARECODES) {
     shareCodes = process.env.DDFACTORY_SHARECODES.split();
   }
 } else {
-  console.log(`由于您环境变量(DDFACTORY_SHARECODES)里面未提供助力码，故此处运行将会给脚本内置的码进行助力，请知晓！`)
+  console.log(`去设置变量(DDFACTORY_SHARECODES)不同好友中间用@符号隔开`)
 }
 for (let i = 0; i < shareCodes.length; i++) {
   const index = (i + 1 === 1) ? '' : (i + 1);
