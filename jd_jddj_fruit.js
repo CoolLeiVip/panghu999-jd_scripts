798d258e427a57beb280c958f6c70805bf31e7ca
panghu999 <51199175+panghu999@users.noreply.github.com>
2021-06-27 16:48:12 +0800

    Update jd_jddj_fruit.js

--- jd_jddj_fruit.js
+++ jd_jddj_fruit.js
@@ -1,17 +1,3 @@
-/*
-v5.6
-京东到家果园任务脚本,支持qx,loon,shadowrocket,surge,nodejs
-兼容京东jdCookie.js
-手机设备在boxjs里填写cookie
-boxjs订阅地址:https://gitee.com/passerby-b/javascript/raw/master/JD/passerby-b.boxjs.json
-
-[task_local]
-10 0,3,8,11,17 * * * https://raw.githubusercontent.com/passerby-b/JDDJ/main/jddj_fruit.js
-
-[Script]
-cron "10 0,3,8,11,17 * * *" script-path=https://raw.githubusercontent.com/passerby-b/JDDJ/main/jddj_fruit.js,tag=京东到家果园任务
-
-*/
 let isNotify = true;//是否通知,仅限nodejs
 let ckPath = './jdCookie.js';//ck路径,环境变量:JDDJ_CKPATH
 const $ = new API("jd_jddj_fruit");
@@ -20,10 +6,6 @@ let lat = '30.' + Math.round(Math.random() * (99999 - 10000) + 10000);
 let lng = '114.' + Math.round(Math.random() * (99999 - 10000) + 10000);
 let cityid = Math.round(Math.random() * (1500 - 1000) + 1000);
 let cookies = [], notify = '';
-let scodes = [], jddjyqm = '';
-if (process.env.jddjyqm) {
-  jddjyqm = process.env.jddjyqm;
-}
 waterNum = 0, waterTimes = 0, shareCode = '', hzstr = '', msgStr = '';
 !(async() => {
     if (cookies.length == 0) {
@@ -79,7 +61,7 @@ waterNum = 0, waterTimes = 0, shareCode = '', hzstr = '', msgStr = '';
         await $.wait(1000);
         await runTask(tslist);
         await $.wait(1000);
-        //await zhuLi();
+        await zhuLi();
         await $.wait(1000);
         await water();
         await $.wait(1000);
@@ -90,25 +72,6 @@ waterNum = 0, waterTimes = 0, shareCode = '', hzstr = '', msgStr = '';
             if (element.taskId == '23eee1c043c01bc') {
                 shareCode += '@' + element.uniqueId + ',';
                 console.log('\n好友互助码:' + shareCode);
-             if(typeof jddjyqm == "undefined" || jddjyqm == null || jddjyqm == ""){
-             console.log(`请去变量设置邀请码格式如\nexport jddjyqm="JD_b38423d1e063000@2413a5236b088e6,"`)
-                 
-             }else {
-             jddjyqm = jddjyqm.replace(/ /g, '').replace(/\n/g, '');
-            if ( !! jddjyqm) {
-                jddjyqm = jddjyqm.substr(0, jddjyqm.length - 1);
-                scodes = jddjyqm.split(',')
-            }
-            for (let index = 0; index < scodes.length; index++) {
-                let option = urlTask('https://daojia.jd.com/client?lat=' + lat + '&lng=' + lng + '&lat_pos=' + lat + '&lng_pos=' + lng + '&city_id=' + cityid + '&deviceToken=' + deviceid + '&deviceId=' + deviceid + '&channel=wx_xcx&mpChannel=wx_xcx&platform=5.0.0&platCode=mini&appVersion=5.0.0&appName=paidaojia&deviceModel=appmodel&xcxVersion=9.2.0&isNeedDealError=true&business=djgyzhuli&functionId=task%2Ffinished&body=%7B%22modelId%22%3A%22M10007%22%2C%22taskType%22%3A1201%2C%22taskId%22%3A%2223eee1c043c01bc%22%2C%22plateCode%22%3A5%2C%22assistTargetPin%22%3A%22' + scodes[index].split('@')[0] + '%22%2C%22uniqueId%22%3A%22' + scodes[index].split('@')[1] + '%22%7D', '');
-                await $.http.get(option).then(response => {
-                    let data = JSON.parse(response.body);
-                    console.log('\n【A助力】:' + data.msg)
-                });
-              
-            }
-             }
-
                 hzstr = ',助力' + element.finishNum + '/' + element.totalNum + ',助力你的好友:';
                 if (element.fissionUserInfoList && element.fissionUserInfoList.length > 0) {
                     element.fissionUserInfoList.forEach(item => {
@@ -136,24 +99,8 @@ waterNum = 0, waterTimes = 0, shareCode = '', hzstr = '', msgStr = '';
         notify.sendNotify('京东到家果园', '?失败! 原因:' + e + '!')
     }
 }).finally(() => {
-    $.done();
+$.done();
 })
-
-async function zhuLi(shrae) {
-           jddjyqm = jddjyqm.replace(/ /g, '').replace(/\n/g, '');
-            if ( !! jddjyqm) {
-                jddjyqm = jddjyqm.substr(0, jddjyqm.length - 1);
-                scodes = jddjyqm.split(',')
-            }
-            for (let index = 0; index < scodes.length; index++) {
-                let option = urlTask('https://daojia.jd.com/client?lat=' + lat + '&lng=' + lng + '&lat_pos=' + lat + '&lng_pos=' + lng + '&city_id=' + cityid + '&deviceToken=' + deviceid + '&deviceId=' + deviceid + '&channel=wx_xcx&mpChannel=wx_xcx&platform=5.0.0&platCode=mini&appVersion=5.0.0&appName=paidaojia&deviceModel=appmodel&xcxVersion=9.2.0&isNeedDealError=true&business=djgyzhuli&functionId=task%2Ffinished&body=%7B%22modelId%22%3A%22M10007%22%2C%22taskType%22%3A1201%2C%22taskId%22%3A%2223eee1c043c01bc%22%2C%22plateCode%22%3A5%2C%22assistTargetPin%22%3A%22' + scodes[index].split('@')[0] + '%22%2C%22uniqueId%22%3A%22' + scodes[index].split('@')[1] + '%22%7D', '');
-                await $.http.get(option).then(response => {
-                    let data = JSON.parse(response.body);
-                    console.log('\n【助力】:' + data.msg)
-                });
-              
-            }
-}
 async function userinfo() {
     return new Promise(async resolve => {
         try {
@@ -192,7 +139,6 @@ async function taskList() {
         }
     })
 }
-
 async function water() {
     return new Promise(async resolve => {
         try {
@@ -265,6 +211,35 @@ async function waterBottle() {
             resolve()
         } catch (error) {
             console.log('\n【收玻璃瓶水滴】:' + error);
+            resolve()
+        }
+    })
+}
+async function zhuLi() {
+    return new Promise(async resolve => {
+        try {
+            let scodes = [], codestr = '';
+         
+                if ($.read('shareCodes')) 
+                codestr += $.read('shareCodes')
+            
+            
+            codestr = codestr.replace(/ /g, '').replace(/\n/g, '');
+            if ( !! codestr) {
+                codestr = codestr.substr(0, codestr.length - 1);
+                scodes = codestr.split(',')
+            }
+            for (let index = 0; index < scodes.length; index++) {
+                let option = urlTask('https://daojia.jd.com/client?lat=' + lat + '&lng=' + lng + '&lat_pos=' + lat + '&lng_pos=' + lng + '&city_id=' + cityid + '&deviceToken=' + deviceid + '&deviceId=' + deviceid + '&channel=wx_xcx&mpChannel=wx_xcx&platform=5.0.0&platCode=mini&appVersion=5.0.0&appName=paidaojia&deviceModel=appmodel&xcxVersion=9.2.0&isNeedDealError=true&business=djgyzhuli&functionId=task%2Ffinished&body=%7B%22modelId%22%3A%22M10007%22%2C%22taskType%22%3A1201%2C%22taskId%22%3A%2223eee1c043c01bc%22%2C%22plateCode%22%3A5%2C%22assistTargetPin%22%3A%22' + scodes[index].split('@')[0] + '%22%2C%22uniqueId%22%3A%22' + scodes[index].split('@')[1] + '%22%7D', '');
+                await $.http.get(option).then(response => {
+                    let data = JSON.parse(response.body);
+                    console.log('\n【助力】:' + data.msg)
+                });
+                await $.wait(1000)
+            }
+            resolve()
+        } catch (error) {
+            console.log('\n【助力】:' + error);
             resolve()
         }
     })
@@ -532,5 +507,3 @@ function _uuid() {
     return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4()
 }
 function ENV() { const e = "undefined" != typeof $task, t = "undefined" != typeof $loon, s = "undefined" != typeof $httpClient && !t, i = "function" == typeof require && "undefined" != typeof $jsbox; return { isQX: e, isLoon: t, isSurge: s, isNode: "function" == typeof require && !i, isJSBox: i, isRequest: "undefined" != typeof $request, isScriptable: "undefined" != typeof importModule } } function HTTP(e = { baseURL: "" }) { const { isQX: t, isLoon: s, isSurge: i, isScriptable: n, isNode: o } = ENV(), r = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/\/=]*)/; const u = {}; return ["GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH"].forEach(l => u[l.toLowerCase()] = (u => (function (u, l) { l = "string" == typeof l ? { url: l } : l; const h = e.baseURL; h && !r.test(l.url || "") && (l.url = h ? h + l.url : l.url); const a = (l = { ...e, ...l }).timeout, c = { onRequest: () => { }, onResponse: e => e, onTimeout: () => { }, ...l.events }; let f, d; if (c.onRequest(u, l), t) f = $task.fetch({ method: u, ...l }); else if (s || i || o) f = new Promise((e, t) => { (o ? require("request") : $httpClient)[u.toLowerCase()](l, (s, i, n) => { s ? t(s) : e({ statusCode: i.status || i.statusCode, headers: i.headers, body: n }) }) }); else if (n) { const e = new Request(l.url); e.method = u, e.headers = l.headers, e.body = l.body, f = new Promise((t, s) => { e.loadString().then(s => { t({ statusCode: e.response.statusCode, headers: e.response.headers, body: s }) }).catch(e => s(e)) }) } const p = a ? new Promise((e, t) => { d = setTimeout(() => (c.onTimeout(), t(`${u} URL: ${l.url} exceeds the timeout ${a} ms`)), a) }) : null; return (p ? Promise.race([p, f]).then(e => (clearTimeout(d), e)) : f).then(e => c.onResponse(e)) })(l, u))), u } function API(e = "untitled", t = !1) { const { isQX: s, isLoon: i, isSurge: n, isNode: o, isJSBox: r, isScriptable: u } = ENV(); return new class { constructor(e, t) { this.name = e, this.debug = t, this.http = HTTP(), this.env = ENV(), this.node = (() => { if (o) { return { fs: require("fs") } } return null })(), this.initCache(); Promise.prototype.delay = function (e) { return this.then(function (t) { return ((e, t) => new Promise(function (s) { setTimeout(s.bind(null, t), e) }))(e, t) }) } } initCache() { if (s && (this.cache = JSON.parse($prefs.valueForKey(this.name) || "{}")), (i || n) && (this.cache = JSON.parse($persistentStore.read(this.name) || "{}")), o) { let e = "root.json"; this.node.fs.existsSync(e) || this.node.fs.writeFileSync(e, JSON.stringify({}), { flag: "wx" }, e => console.log(e)), this.root = {}, e = `${this.name}.json`, this.node.fs.existsSync(e) ? this.cache = JSON.parse(this.node.fs.readFileSync(`${this.name}.json`)) : (this.node.fs.writeFileSync(e, JSON.stringify({}), { flag: "wx" }, e => console.log(e)), this.cache = {}) } } persistCache() { const e = JSON.stringify(this.cache, null, 2); s && $prefs.setValueForKey(e, this.name), (i || n) && $persistentStore.write(e, this.name), o && (this.node.fs.writeFileSync(`${this.name}.json`, e, { flag: "w" }, e => console.log(e)), this.node.fs.writeFileSync("root.json", JSON.stringify(this.root, null, 2), { flag: "w" }, e => console.log(e))) } write(e, t) { if (this.log(`SET ${t}`), -1 !== t.indexOf("#")) { if (t = t.substr(1), n || i) return $persistentStore.write(e, t); if (s) return $prefs.setValueForKey(e, t); o && (this.root[t] = e) } else this.cache[t] = e; this.persistCache() } read(e) { return this.log(`READ ${e}`), -1 === e.indexOf("#") ? this.cache[e] : (e = e.substr(1), n || i ? $persistentStore.read(e) : s ? $prefs.valueForKey(e) : o ? this.root[e] : void 0) } delete(e) { if (this.log(`DELETE ${e}`), -1 !== e.indexOf("#")) { if (e = e.substr(1), n || i) return $persistentStore.write(null, e); if (s) return $prefs.removeValueForKey(e); o && delete this.root[e] } else delete this.cache[e]; this.persistCache() } notify(e, t = "", l = "", h = {}) { const a = h["open-url"], c = h["media-url"]; if (s && $notify(e, t, l, h), n && $notification.post(e, t, l + `${c ? "\n多媒体:" + c : ""}`, { url: a }), i) { let s = {}; a && (s.openUrl = a), c && (s.mediaUrl = c), "{}" === JSON.stringify(s) ? $notification.post(e, t, l) : $notification.post(e, t, l, s) } if (o || u) { const s = l + (a ? `\n点击跳转: ${a}` : "") + (c ? `\n多媒体: ${c}` : ""); if (r) { require("push").schedule({ title: e, body: (t ? t + "\n" : "") + s }) } else console.log(`${e}\n${t}\n${s}\n\n`) } } log(e) { this.debug && console.log(`[${this.name}] LOG: ${this.stringify(e)}`) } info(e) { console.log(`[${this.name}] INFO: ${this.stringify(e)}`) } error(e) { console.log(`[${this.name}] ERROR: ${this.stringify(e)}`) } wait(e) { return new Promise(t => setTimeout(t, e)) } done(e = {}) { console.log('done!'); s || i || n ? $done(e) : o && !r && "undefined" != typeof $context && ($context.headers = e.headers, $context.statusCode = e.statusCode, $context.body = e.body) } stringify(e) { if ("string" == typeof e || e instanceof String) return e; try { return JSON.stringify(e, null, 2) } catch (e) { return "[object Object]" } } }(e, t) }
-/*****************************************************************************/
-
