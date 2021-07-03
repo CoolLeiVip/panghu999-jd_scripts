82a8d5779383c52999c86ab9fc963fa1c1cebeb1
panghu999 <51199175+panghu999@users.noreply.github.com>
2021-07-04 01:48:31 +0800

    Update jd_bean_home.js

--- jd_bean_home.js
+++ jd_bean_home.js
@@ -26,7 +26,7 @@ const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 let jdNotify = true;//是否关闭通知，false打开通知推送，true关闭通知推送
-const helpAuthor = true; // 是否帮助作者助力，false打开通知推送，true关闭通知推送
+const helpAuthor = false; // 是否帮助作者助力，false打开通知推送，true关闭通知推送
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '', message;
 if ($.isNode()) {
@@ -68,18 +68,18 @@ const JD_API_HOST = 'https://api.m.jd.com/';
       await jdBeanHome();
     }
   }
-  // for (let i = 0; i < cookiesArr.length; i++) {
-  //   $.index = i + 1;
-  //   if (cookiesArr[i]) {
-  //     $.UserName = decodeURIComponent(cookie.match(/pt_pin=([^; ]+)(?=;?)/) && cookie.match(/pt_pin=([^; ]+)(?=;?)/)[1])
-  //     cookie = cookiesArr[i];
-  //     if ($.newShareCodes.length > 1) {
-  //       console.log(`\n【抢京豆】 ${$.UserName} 去助力排名第一的cookie`);
+ for (let i = 0; i < cookiesArr.length; i++) {
+   $.index = i + 1;
+   if (cookiesArr[i]) {
+     $.UserName = decodeURIComponent(cookie.match(/pt_pin=([^; ]+)(?=;?)/) && cookie.match(/pt_pin=([^; ]+)(?=;?)/)[1])
+     cookie = cookiesArr[i];
+     if ($.newShareCodes.length > 1) {
+       console.log(`\n【抢京豆】 ${$.UserName} 去助力排名第一的cookie`);
   //       // let code = $.newShareCodes[(i + 1) % $.newShareCodes.length]
-  //       // await help(code[0], code[1])
-  //       let code = $.newShareCodes[0];
-  //       await help(code[0], code[1]);
-  //     }
+       // await help(code[0], code[1])
+       let code = $.newShareCodes[0];
+       await help(code[0], code[1]);
+     }
   //     if (helpAuthor && $.authorCode) {
   //       console.log(`\n【抢京豆】${$.UserName} 去帮助作者`)
   //       for (let code of $.authorCode) {
@@ -89,21 +89,21 @@ const JD_API_HOST = 'https://api.m.jd.com/';
   //         }
   //       }
   //     }
-  //     if (helpAuthor && $.authorCode2) {
-  //       for (let code of $.authorCode2) {
-  //         const helpRes = await help(code.shareCode, code.groupCode);
-  //         if (helpRes && helpRes.data.respCode === 'SG209') {
-  //           break;
-  //         }
-  //       }
-  //     }
-  //     for (let j = 1; j < $.newShareCodes.length; j++) {
-  //       console.log(`【抢京豆】${$.UserName} 去助力账号 ${j + 1}`)
-  //       let code = $.newShareCodes[j];
-  //       await help(code[0], code[1])
-  //     }
-  //   }
-  // }
+     if (helpAuthor && $.authorCode2) {
+       for (let code of $.authorCode2) {
+         const helpRes = await help(code.shareCode, code.groupCode);
+         if (helpRes && helpRes.data.respCode === 'SG209') {
+           break;
+         }
+       }
+     }
+     for (let j = 1; j < $.newShareCodes.length; j++) {
+       console.log(`【抢京豆】${$.UserName} 去助力账号 ${j + 1}`)
+       let code = $.newShareCodes[j];
+       await help(code[0], code[1])
+     }
+   }
+ }
 })()
   .catch((e) => {
     $.log('', `❌ ${$.name}, 失败! 原因: ${e}!`, '')
